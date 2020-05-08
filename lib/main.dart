cdfd420bce6db98de7eebdf019cee34b9615550b
Parth Maniar <officialparthmaniar@gmail.com>
2020-05-08 15:50:41 +0530

    🎨 Settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
-import 'package:flutter/material.dart';
 import 'package:theme_provider/util/theme_notifier.dart';
+import 'package:theme_provider/values/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'screens/home.dart';
+
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]).then((_) {
+    SharedPreferences.getInstance().then((prefs) {
+      var darkModeOn = prefs.getBool('darkMode') ?? true;
+      runApp(
+        ChangeNotifierProvider<ThemeNotifier>(
+          create: (_) => ThemeNotifier(darkModeOn ? darkTheme : lightTheme),
+          child: MyApp(),
+        ),
+      );
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
       title: 'Theme Provider',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: themeNotifier.getTheme(),
       home: HomePage(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:theme_provider/screens/settings.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Theme Provider"),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0), // here the desired height
+          child: AppBar(
+            title: Text("Theme Provider"),
         centerTitle: true,
-        elevation: 0.0,
-      ),
+            // ...
+          )),
+      extendBody: true,
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -21,6 +38,49 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ],
+        ),),
+      floatingActionButton: Container(
+        height: 56.0,
+        child: Card(
+//        height: 56.0,
+//        decoration: BoxDecoration(
+//          color: Theme.of(context).accentColor,
+//          borderRadius: BorderRadius.circular(50.0),
+//        ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          color: Theme.of(context).accentColor,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[  
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Container(
+                  width: 1,
+                  height: double.infinity,
+                  color: Theme.of(context).dividerColor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.settings,
+                    color: Theme.of(context).accentIconTheme.color,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SettingsPage(),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
